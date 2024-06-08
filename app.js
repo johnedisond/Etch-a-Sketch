fe3e0ab18748831a050a4897a76fdc4b656aa547
Eds Ibalio <johnedisond.ibalio@gmail.com>
2024-06-09 01:27:55 +0800

    add pen function through keyboard event

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let container = document.querySelector(".container");
+let pen = document.querySelector(".pen");
 
 let color = "black";
+let penActive = false;
 
 
 function gridSize(size) {
-
     let box = container.querySelectorAll("div");
 
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -37,8 +38,11 @@ function getSize() {
 
 size.addEventListener("click", () => {
     let size = getSize();
+
     gridSize(size);
     resetColors();
+    penActive = false;
+    pen.textContent = "PEN UP";
 })
 
 
@@ -54,17 +58,34 @@ function resetColors() {
 // setting grid color----------
 
 function setColor() {
-    if (color === "random") {
-        const r = Math.floor(Math.random() * 256);
-        const g = Math.floor(Math.random() * 256);
-        const b = Math.floor(Math.random() * 256);
-        this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    if (penActive) {
+        if (color === "random") {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 
-    } else {
-        this.style.backgroundColor = color;
+        } else {
+            this.style.backgroundColor = color;
+        }
     }
 }
 
 function makeColor(colorChoice) {
     color = colorChoice;
 }
+
+addEventListener("keydown", (e) => {
+    console.log(e);
+    if (e.key === " ") {
+        penActive = !penActive;
+        if (penActive) {
+            pen.textContent = "PEN DOWN";
+            e.preventDefault();
+
+        } else {
+            pen.textContent = "PEN UP";
+            e.preventDefault();
+        }
+    }
+})
